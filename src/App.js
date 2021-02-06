c06ecd0e616213006fc6eb7db744edc263e35d78
Paul <paul@live-eo.com>
2021-02-06 13:10:42 +0100

    added fucntion to fetch data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
+import { useEffect, useState } from 'react';
+import UserList from './components/UserList/UserList';
 import './App.css';
 
+const fetchData = (type) => {
+  return fetch(`https://jsonplaceholder.typicode.com/${type}`)
+    .then(response => response.json())
+}
+
 function App() {
+  const [users, setUsers] = useState([])
+
+  useEffect(() => {
+    const getUsers = async() => {
+      let users = await fetchData('users')
+      setUsers(users)
+    }
+
+    getUsers()
+  }, [])
+
   return (
     <div className="App">
-
+      <UserList users={users} />
     </div>
   );
 }
