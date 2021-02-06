aa448adef1e4555a75d3729af79e3f80a00f7058
Paul <paul@live-eo.com>
2021-02-06 17:36:53 +0100

    Squashed commit of the following:
    commit 5d86eedb3031ec66a191084773b32b31dc747b9a
    Author: Paul <paul@live-eo.com>
    Date:   Sat Feb 6 17:36:04 2021 +0100
    
        Added useMemo and React.memo to UserList
    
    commit c06ecd0e616213006fc6eb7db744edc263e35d78
    Author: Paul <paul@live-eo.com>
    Date:   Sat Feb 6 13:10:42 2021 +0100
    
        added fucntion to fetch data
    
    commit c8dc301414d4600d689244e409a419fe03f08f4f
    Author: Paul <paul@live-eo.com>
    Date:   Sat Feb 6 13:10:29 2021 +0100
    
        setup UserList component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
+import { useEffect, useState, useMemo } from 'react';
+import UserList from './components/UserList/UserList';
 import './App.css';
 
+const fetchData = (type) => {
+  return fetch(`https://jsonplaceholder.typicode.com/${type}`)
+    .then(response => response.json())
+}
+
 function App() {
+  const [users, setUsers] = useState([])
+
+  const usersRef = useMemo(() => users, [users])
+
+  useEffect(() => {
+    const getUsers = async() => {
+      let users = await fetchData('users')
+      setUsers(users)
+    }
+
+    getUsers()
+  }, [])
+
   return (
     <div className="App">
-
+      <UserList users={usersRef} />
     </div>
   );
 }
---
+++ src/components/UserList/UserList.js
@@ -0,0 +1,13 @@
+import {memo} from 'react'
+import classes from './UserList.module.css';
+
+const UserList = ({users}) => {
+    console.log(users)
+  return (
+    <div className={classes.container}>
+        Users
+    </div>
+  );
+}
+
+export default memo(UserList);
