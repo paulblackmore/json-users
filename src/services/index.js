2d60b1f199ee4bceb0778236aa45fe3c643071ec
Paul <paul@live-eo.com>
2021-02-07 15:27:22 +0100

    added full CRUD to serices with headers

--- src/services/index.js
+++ src/services/index.js
@@ -1,26 +1,36 @@
 const get = (type) => {
-  return fetch(`https://jsonplaceholder.typicode.com/${type}`)
-    .then(response => response.json())
+  return fetch(`https://jsonplaceholder.typicode.com/${type}`).then(response => response.json());
 }
 
-const create = (type) => {
-  return fetch(`https://jsonplaceholder.typicode.com/${type}`)
-    .then(response => response.json())
+const create = (body) => {
+  const headers = {
+    method: 'POST',
+    body: JSON.stringify(body),
+    headers: {'Content-type': 'application/json; charset=UTF-8'}
+  }
+
+  return fetch('https://jsonplaceholder.typicode.com/users', headers).then((response) => response.json());
 }
 
-const update = (type) => {
-  return fetch(`https://jsonplaceholder.typicode.com/${type}`)
-    .then(response => response.json())
+const update = (body) => {
+  const headers = {
+    method: 'PUT',
+    body: JSON.stringify(body),
+    headers: {'Content-type': 'application/json; charset=UTF-8'}
+  }
+
+  return fetch(`https://jsonplaceholder.typicode.com/users`, headers).then(response => response.json());
 }
 
-const remove = (type) => {
-  return fetch(`https://jsonplaceholder.typicode.com/${type}`)
-    .then(response => response.json())
+const destroy = () => {
+  const headers = {
+    method: 'DELETE'
+  }
+  return fetch(`https://jsonplaceholder.typicode.com/users`, headers).then(response => response.json());
 }
-
 export const userService = {
   get,
   create,
   update,
-  remove
+  destroy
 }
