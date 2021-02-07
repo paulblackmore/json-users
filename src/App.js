27918350e37493399c20fb4cca3b4137473f182f
Paul <paul@live-eo.com>
2021-02-07 15:28:23 +0100

    Squashed commit of the following:
    commit 2d60b1f199ee4bceb0778236aa45fe3c643071ec
    Author: Paul <paul@live-eo.com>
    Date:   Sun Feb 7 15:27:22 2021 +0100
    
        added full CRUD to serices with headers
    
    commit 0dfc53fb24d07ad9e747ecdb9fd24f0743af3c03
    Author: Paul <paul@live-eo.com>
    Date:   Sun Feb 7 15:20:07 2021 +0100
    
        added remove functiont o services
    
    commit c7a77afbb685c565137cb1d7f4e28c18572c9528
    Author: Paul <paul@live-eo.com>
    Date:   Sun Feb 7 15:18:21 2021 +0100
    
        added services folder with get,create, and update

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
-import { useEffect, useState, useMemo } from 'react';
+import {useEffect, useState, useMemo} from 'react';
+import {userService} from './services';
 import UserList from './components/UserList/UserList';
 import './App.css';
 
-const fetchData = (type) => {
-  return fetch(`https://jsonplaceholder.typicode.com/${type}`)
-    .then(response => response.json())
-}
-
 function App() {
   const [users, setUsers] = useState([])
-
+  
   const usersRef = useMemo(() => users, [users])
 
   useEffect(() => {
     const getUsers = async() => {
-      let users = await fetchData('users')
+      let users = await userService.get('users')
       setUsers(users)
     }
 
